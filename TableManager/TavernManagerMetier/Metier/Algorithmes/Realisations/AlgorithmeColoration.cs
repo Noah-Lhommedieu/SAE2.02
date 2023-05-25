c51e1d6bbe8c8cdde0e2972bfbd202a2200d14f2
Noah Lhommedieu <115083482+Noah-Lhommedieu@users.noreply.github.com>
2023-05-25 17:00:43 +0200

    Correction Git Error

--- TableManager/TavernManagerMetier/Metier/Algorithmes/Realisations/AlgorithmeColoration.cs
+++ TableManager/TavernManagerMetier/Metier/Algorithmes/Realisations/AlgorithmeColoration.cs
@@ -15,9 +15,157 @@ public class AlgorithmeColoration : IAlgorithme
     public string Nom => "Coloration Amis";
 
     public long TempsExecution => tempsExecution;
+<<<<<<< HEAD
     public void Executer(Taverne taverne)
     {
         try
+=======
+
+
+    public void Executer(Taverne taverne)
+    {
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+        Graphe graphe = new Graphe(taverne);
+
+
+        List<int> CouleursSommets = new List<int>();
+        List<Sommet> ListSommets = new List<Sommet>();
+        int couleur = 0;
+
+        foreach (Sommet sommet in graphe.Sommets)
+        {
+            ListSommets.Add(sommet);
+        }
+
+        foreach (Sommet sommet in ListSommets)
+        {
+            sommet.Couleur = -1;
+        }
+
+        foreach (Sommet sommet in ListSommets)
+        {
+            couleur = 0;
+            bool estEnnemi = false;
+            while (!estEnnemi)
+            {
+                estEnnemi = true;
+                foreach (Sommet voisin in sommet.Voisin)
+                {
+                    if (voisin.Couleur == couleur)
+                    {
+                        estEnnemi = false;
+                    }
+                }
+                if (estEnnemi && sommet.Couleur == -1)
+                {
+                    sommet.Couleur = couleur;
+                    CouleursSommets.Add(couleur);                       
+                }
+                else
+                {
+                    couleur += 1;
+                }
+            }
+        }
+
+        for(int i = 0; i < taverne.Tables.Length; i++)
+        {
+            taverne.Tables[i].Couleur = CouleursSommets[i];
+        }
+
+        for (int i = 0; i <= CouleursSommets.Max(); i++)
+        {
+            taverne.AjouterTable();
+        }
+
+        
+        foreach (Client client in taverne.Clients)
+        {
+            int TableClientNbActuel = 0;
+
+            Sommet sommetDuClient = graphe.DicoSOMMET[client];
+            TableClientNbActuel += sommetDuClient.NbClients;
+            try
+            {
+                Table table = sommetDuClient.GetTableAssocie(taverne);
+                int test = sommetDuClient.GetTableAssocie(taverne).NombreClients;
+                if (sommetDuClient.GetTableAssocie(taverne).NombreClients + sommetDuClient.NbClients > taverne.CapactieTables) // Si 
+                
+                taverne.AjouterClientTable(client.Numero, sommetDuClient.Couleur);
+            }
+            catch
+            {
+                taverne.AjouterTable();
+                taverne.AjouterClientTable(client.Numero, sommetDuClient.Couleur+1);
+            }
+            
+            /*if(client.Table.NombreClients > client.Table.Capacite)
+            {
+                
+            }*/
+            
+
+
+        }
+        sw.Stop();
+        this.tempsExecution = sw.ElapsedMilliseconds;
+
+    }
+    /*if(client.Table.Clients.Length + sommetDuClient.NbClients > client.Table.Capacite)
+            {
+                taverne.AjouterTable();
+                taverne.AjouterClientTable(client.Numero, sommetDuClient.Couleur + 1);
+            }
+            else
+            {
+                taverne.AjouterClientTable(client.Numero, sommetDuClient.Couleur);
+            }
+
+            if (taverne.Clients[client.Table.].Table.Clients.Length + sommetDuClient.NbClients > taverne.Clients[sommetDuClient.Couleur].Table.Capacite)
+            {
+                taverne.AjouterTable();
+                taverne.AjouterClientTable(client.Numero, sommetDuClient.Couleur + 1);
+            }
+            else
+            {
+                taverne.AjouterClientTable(client.Numero, sommetDuClient.Couleur);
+            }*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*public void Executer(Taverne taverne)
+    {
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+
+        // Création du graphe à partir de la taverne
+        Graphe graphe = new Graphe(taverne);
+
+        // Tri (pas de tri ici car on prend l'ordre dans lequel on donne)
+
+        // Dictionnaire qui associe un Sommet à sa couleur
+        Dictionary<Sommet, int> sommetEtLeurCouleur = new Dictionary<Sommet, int>();
+
+        // Initialisation des sommets avec une couleur
+        foreach (Sommet sommet in graphe.Sommets)
+>>>>>>> 303b859ecd42eb9f3463711d8d7f77a92e458d9e
         {
             ExecuterAlgo(taverne);
         }
@@ -32,6 +180,16 @@ public class AlgorithmeColoration : IAlgorithme
     {
         try
         {
+<<<<<<< HEAD
+=======
+            if (numTable == couleur)
+                occurrence++;
+        }
+        return occurrence;
+    }*/
+}
+/*Code de lorenzo
+>>>>>>> 303b859ecd42eb9f3463711d8d7f77a92e458d9e
 
             Stopwatch sw = new Stopwatch();
             sw.Start();
